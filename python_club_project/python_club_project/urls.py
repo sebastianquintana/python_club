038d457bf8e19b12a9af44da34d17ad34a0f9dc2
sebastianquintana <sebastianquintan@gmail.com>
2020-04-30 12:09:09 -0700

    urls error

--- python_club_project/python_club_project/urls.py
+++ python_club_project/python_club_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pythonclubapp/',include('pythonclubapp.utls')),
+    path('pythonclubapp/',include('pythonclubapp.urls')),
 ]
