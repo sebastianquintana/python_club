55b019fb9c96ce7c6b4d7397c1a6b689a6b19e89
sebastianquintana <sebastianquintan@gmail.com>
2020-04-30 18:05:54 -0700

    htm

--- python_club_project/pythonclubapp/views.py
+++ python_club_project/pythonclubapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    return render(request, 'pythonclubapp/index.html')
+    return render(request, 'pythonclubapp/index.htm')
