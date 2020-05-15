0c74d4676f58641c061e727ce92a14f25e68094a
sebastianquintana <sebastianquintan@gmail.com>
2020-05-15 12:35:59 -0700

    add models to in admin,py

--- python_club_project/pythonclubapp/admin.py
+++ python_club_project/pythonclubapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import ProductType, Product, Review
+
+#register the models.
+admin.site.register(ProductType)
+admin.site.register(Product)
+admin.site.register(Review)
