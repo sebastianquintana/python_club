238b9318fda8df2680daf1cd666d26293ae162d2
sebastianquintana <sebastianquintan@gmail.com>
2020-05-16 11:13:01 -0700

    create the view and add url to urls.py and the templete i create a new file types.htm

--- python_club_project/pythonclubapp/views.py
+++ python_club_project/pythonclubapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.from django.shortcuts import render
+from.models import ProductType, Product, Review
 
+def gettypes(request):
+    type_list=ProductType,objects.all()
+    return render(request, 'techapp/types.htm' , {'type_list': type_list})
 # Create your views here.
 def index (request):
     return render(request, 'pythonclubapp/index.htm')
