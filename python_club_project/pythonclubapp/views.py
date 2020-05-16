561fbce6a3f91d22e52678825f0e799ecce3e79c
sebastianquintana <sebastianquintan@gmail.com>
2020-05-16 12:50:29 -0700

    create product template do the registration add a menu link  and create a details views with registration ulr for both

--- python_club_project/pythonclubapp/views.py
+++ python_club_project/pythonclubapp/views.py
@@ -9,3 +9,18 @@ def gettypes(request):
 # Create your views here.
 def index (request):
     return render(request, 'pythonclubapp/index.htm')
+
+def getproducts(request):
+    products_list=Product.objects.all()
+    return render(request, 'pythonclubapp/products.htm', {'products_list': products_list})
+
+def productdetails(request, id):
+    prod=get_object_or_404(Product, pk=id)
+    discount=prod.memberdiscount
+    reviews=Review.objects.filter(product=id).count()
+    context={
+        'prod' : prod,
+        'discount' : discount,
+        'reviews' : reviews,
+    }
+    return render(request, 'pythonclubapp/proddetails.htm', context=context)
