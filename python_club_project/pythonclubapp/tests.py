44aab9cf802705bf222daf7fd8fc8eac793cb9e4
sebastianquintana <sebastianquintan@gmail.com>
2020-06-01 12:56:13 -0700

    test all the models succesfull

--- python_club_project/pythonclubapp/tests.py
+++ python_club_project/pythonclubapp/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .models import Product, ProductType, Review
 
-# Create your tests here.
+class ProductTypeTest(TestCase):
+   def test_string(self):
+       type=ProductType(typename="Tablet")
+       self.assertEqual(str(type), type.typename)
+
+def test_table(self):
+     self.assertEqual(str(productType._meta.db_table), 'producttype')
+
+class ProductTest(TestCase):
+   #set up one time sample data
+   def setup(self):
+       type = ProductType(typename='laptop')
+       product=Product(productname='Lenovo', producttype=type, productprice='500.00')
+       return product
+   def test_string(self):
+       prod = self.setup()
+       self.assertEqual(str(prod), prod.productname)
+  
+   #test the discount property
+   def test_discount(self):
+       prod=self.setup()
+       self.assertEqual(prod.memberdiscount(), 25.00)
+
+   def test_type(self):
+       prod=self.setup()
+       self.assertEqual(str(prod.producttype), 'laptop')
+
+   def test_table(self):
+       self.assertEqual(str(Product._meta.db_table), 'product')
+
+
+
+class ReviewTest(TestCase):
+   def test_string(self):
+       rev=Review(reviewtitle="Best Review")
+       self.assertEqual(str(rev), rev.reviewtitle)
+
+   def test_table(self):
+       self.assertEqual(str(Review._meta.db_table), 'review')
