ceb8542f6e984027c71e827851af0fde2d81697e
sebastianquintana <sebastianquintan@gmail.com>
2020-06-05 16:00:57 -0700

    add a test for a form in tests.py

--- python_club_project/pythonclubapp/tests.py
+++ python_club_project/pythonclubapp/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Product, ProductType, Review
 
+
 class ProductTypeTest(TestCase):
    def test_string(self):
        type=ProductType(typename="Tablet")
@@ -40,3 +41,21 @@ class ReviewTest(TestCase):
 
    def test_table(self):
        self.assertEqual(str(Review._meta.db_table), 'review')
+
+
+class New_Product_authentication_test(TestCase):
+    def setUp(self):
+        self.test_user=User.objects.create_user(username='testuser1', password='P@ssw0rd1')
+        self.type=ProductType.objects.create(typename='laptop')
+        self.prod = Product.objects.create(productname='product1', producttype=self.type, user=self.test_user, productprice=500, productentrydate='2019-04-02',producturl= 'http://www.dell.com', productdescription="a product")
+
+    def test_redirect_if_not_logged_in(self):
+        response=self.client.get(reverse('newproduct'))
+        self.assertRedirects(response, '/accounts/login/?next=/pythonclubapp/newProduct/')
+
+    def test_Logged_in_uses_correct_template(self):
+        login=self.client.login(username='testuser1', password='P@ssw0rd1')
+        response=self.client.get(reverse('newproduct'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pythonclubapp/newproduct.htm')
